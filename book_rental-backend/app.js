334aa8176f693ffb28ec82af3ad64eeadf39b7b3
KanVux <khangb2111846@student.ctu.edu.vn>
2024-11-20 02:01:35 +0700

    Cai dat xu ly loi

--- book_rental-backend/app.js
+++ book_rental-backend/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-const BookRouter = require("./app/routes/book.route")
+const BookRouter = require("./app/routes/book.route");
+const ApiError = require("./app/api-errors");
 
 conts
 const app = express();
@@ -9,8 +10,15 @@ app.use(cors())
 app.use(express.json())
 app.use("/api/books", booksRouter);
 
-app.get("/", (req, res) => {
-    res.json({message: "Welcome to book rental application"})
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resource not found"));
+}) 
+
+app.use((err, req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Internal Server Error",
+    });
 });
+
 
 module.exports = app;
