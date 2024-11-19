224983d54e9e5742412cd5c765b41b4c46666987
KanVux <khangb2111846@student.ctu.edu.vn>
2024-11-20 01:53:41 +0700

    Dinh nghia cac route cho tai nguyen Book

--- book_rental-backend/app.js
+++ book_rental-backend/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const cors = require("cors");
+const BookRouter = require("./app/routes/book.route")
 
-
+conts
 const app = express();
 
 app.use(cors())
 app.use(express.json())
+app.use("/api/books", booksRouter);
 
 app.get("/", (req, res) => {
     res.json({message: "Welcome to book rental application"})
